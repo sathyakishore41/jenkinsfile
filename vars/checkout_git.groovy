86f94d4a868840db1ab3306f499c7b4f4dd07e42
sathya <sathyakishore41@gmail.com>
2022-07-19 22:54:11 +0530

    string

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,4 +1,4 @@
-def checkout_git(string repo) {
+def checkout_git(String repo) {
     
     echo "This is for checking out git code."
    git credentialsId: 'sathya', url: 'git@github.com:sathyakishore41/${repo}.git' 
