0e274906dbdd3d459bac1ca79a80a71c6463c9cf
sathya <sathyakishore41@gmail.com>
2022-07-25 21:38:06 +0530

    b

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir /var/lib/jenkins/practice
 cd /var/lib/jenkins/practice
-aws s3 cp s3://samplebucketsathya/java/sparkjava-hello-world-1.0.war .
-scp sparkjava-hello-world-1.0.war ubuntu@10.0.123.147:/home/ubuntu/tomcat/apache-tomcat-9.0.65/webapps
+aws s3 cp s3://samplebucketsathya/java/hello-1.0.war .
+scp hello-1.0.war ubuntu@10.0.123.147:/home/ubuntu/tomcat/apache-tomcat-9.0.65/webapps
 ssh ubuntu@10.0.123.147 << HERE
     cd /home/ubuntu/tomcat/apache-tomcat-9.0.65/bin/
     ./shutdown.sh
