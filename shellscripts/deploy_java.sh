bc0c9aaee94143a2f7ccba663d3398f61a165436
sathya <sathyakishore41@gmail.com>
2022-07-27 20:08:59 +0530

    f

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -7,7 +7,7 @@ IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 mkdir /var/lib/jenkins/deploy
 cd /var/lib/jenkins/deploy
 aws s3 cp s3://samplebucketsathya/java/$tag/sparkjava-hello-world-1.0.war .
-scp sparkjava-hello-world-1.0.war ubuntu@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.65/webapps
+scp sparkjava-hello-world-1.0.war ubuntu@${IP}:/home/ubuntu/tomcat/apache-tomcat-9.0.65/webapps
 ssh ubuntu@${IP} << HERE
     rm -rf /home/ubuntu/tomcat/apache-tomcat-9.0.65/work
     cd /home/ubuntu/tomcat/apache-tomcat-9.0.65/bin/
